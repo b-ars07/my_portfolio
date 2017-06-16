09ae832f6f460e722454dd89a105bf2200f9eb49
Arseniy Berezin <b-ars07@yandex.ru>
2017-06-17 01:06:36 +0300

    Добавил таски генерации спрайтов и копировании файлов

---
+++ gulp/tasks/sprite.png.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function() {
+    $.gulp.task('sprite:png', function() {
+        return $.gulp.src('./source/sprite/*.png')
+            .pipe($.gp.spritesmith({
+                imgName: 'sprite.png',
+                cssName: 'sprite.scss',
+                padding: 5
+            }))
+            .pipe($.gulp.dest($.config.root + '/assets/sprite'));
+    });
+};
