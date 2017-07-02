2e5c874d075a710972937d72645a6fde25fbac30
Arseniy Berezin <b-ars07@yandex.ru>
2017-07-02 21:34:38 +0300

    Добавил flip-эффект для карточки на главной странице

--- source/js/app.js
+++ source/js/app.js
@@ -1,25 +1,48 @@
-var arrow = (function () {
-  return {
-    init: function () {
-        const scrollHeight = $('.js-section-content').offset().top;
+var arrow = (function() {
+    return {
+        init: function() {
+            const scrollHeight = $('.js-section-content').offset().top;
 
-        $('.arrow').on('click', function () {
+            $('.arrow').on('click', function() {
 
-            $('body').animate({
-                scrollTop: scrollHeight
-            }, 1800);
+                $('body').animate({
+                    scrollTop: scrollHeight
+                }, 1800);
 
-            return false;
-        });
+                return false;
+            });
+        }
     }
-  }
 })();
 
-$(function () {
-  if ($('.arrow').length) {
-    arrow.init();
-  }
+$(function() {
+    if ($('.arrow').length) {
+        arrow.init();
+    }
 });
 
+$(function() {
+    var
+        auth_btn = $('.welcome__auth-btn'),
+        main_btn = $('.welcome-menu__link'),
+        card = $('.card__wrapper');
 
+    auth_btn.on('click', function(evt) {
+        evt.preventDefault();
 
+        var _this = $(this);
+
+        _this.toggleClass('active');
+        setTimeout(function() {
+            card.toggleClass('active');
+        }, 400);
+    });
+
+    main_btn.on('click', function(evt) {
+        evt.preventDefault();
+        setTimeout(function() {
+            card.toggleClass('active');
+        }, 400);
+
+    });
+});
