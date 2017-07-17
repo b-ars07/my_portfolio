f28b406b46a5cd13a70888a90a5e8b0ebd7523d7
Arseniy Berezin <b-ars07@yandex.ru>
2017-07-17 19:07:42 +0300

    Доделал админку, отправку письма через форму, вывод из базы данных

--- routes/works.js
+++ routes/works.js
@@ -4,48 +4,50 @@ const nodemailer = require('nodemailer');
 const config = require('../config.json');
 const mongoose = require('mongoose');
 
-router.get('/', function (req, res) {
-  let obj = {
-    title: 'Связаться со мной'
-  };
-  Object.assign(obj, req.app.locals.settings);
-  const Model = mongoose.model('works');
-  Model
-    .find()
-    .then(items => {
-      // обрабатываем шаблон и отправляем его в браузер передаем в шаблон список
-      // записей в блоге
-      Object.assign(obj, {items: items});
-      res.render('pages/works', obj);
-    });
+router.get('/', function(req, res) {
+    let obj = {
+        title: 'Связаться со мной'
+    };
+    Object.assign(obj, req.app.locals.settings);
+    const Model = mongoose.model('works');
+    Model
+        .find()
+        .then(items => {
+            // обрабатываем шаблон и отправляем его в браузер передаем в шаблон список
+            // записей в блоге
+            Object.assign(obj, { items: items });
+            res.render('pages/works', obj);
+        });
 });
 
-router.post('/', function (req, res) {
+router.post('/', function(req, res) {
     //требуем наличия имени, обратной почты и текста
-  if (!req.body.name || !req.body.email || !req.body.text) {
-    //если что-либо не указано - сообщаем об этом
-    return res.json({status: 'Укажите данные!'});
-  }
-    //инициализируем модуль для отправки писем и указываем данные из конфига
-  const transporter = nodemailer.createTransport(config.mail.smtp);
-  const mailOptions = {
-    from: `"${req.body.name}" <${req.body.email}>`,
-    to: config.mail.smtp.auth.user,
-    subject: config.mail.subject,
-    text: req
-    .body
-    .text
-    .trim()
-    .slice(0, 500) + `\n Отправлено с: <${req.body.email}>`
-  };
-  //отправляем почту
-  transporter.sendMail(mailOptions, function (error, info) {
-    //если есть ошибки при отправке - сообщаем об этом
-    if (error) {
-      return res.json({status: 'При отправке письма произошла ошибка'});
+    if (!req.body.name || !req.body.email || !req.body.text) {
+        //если что-либо не указано - сообщаем об этом
+        return res.json({ status: 'Укажите данные!' });
     }
-    res.json({status: 'Письмо успешно отправлено'});
-  });
+    //инициализируем модуль для отправки писем и указываем данные из конфига
+    const transporter = nodemailer.createTransport(config.mail.smtp);
+    const mailOptions = {
+        from: `"${req.body.name}" <${req.body.email}>`,
+        to: config.mail.smtp.auth.user,
+        subject: config.mail.subject,
+        text: req
+            .body
+            .text
+            .trim()
+            .slice(0, 500) + `\n Отправлено с: <${req.body.email}>`
+    };
+    // console.log(transporter);
+    //отправляем почту
+    transporter.sendMail(mailOptions, function(error, info) {
+
+        //если есть ошибки при отправке - сообщаем об этом
+        if (error) {
+            return res.json({ status: 'При отправке письма произошла ошибка ' + error });
+        }
+        res.json({ status: 'Письмо успешно отправлено' });
+    });
 });
 
 module.exports = router;
--- source/js/form.js
+++ source/js/form.js
@@ -1,39 +1,39 @@
-var formMail = document.querySelector('#feedbackForm'),
-    formLogin = document.querySelector('#authForm'),
-    popup = $('#popup');
-popup.hide();
+// var formMail = document.querySelector('#feedbackForm'),
+//     formLogin = document.querySelector('#authForm'),
+//     popup = $('#popup');
+// popup.hide();
 
-if (formMail) {
-    formMail.addEventListener('submit', prepareSendMail);
-}
-if (formLogin) {
-    formLogin.addEventListener('submit', prepareSendLogin);
-}
+// if (formMail) {
+//     formMail.addEventListener('submit', prepareSendMail);
+// }
+// if (formLogin) {
+//     formLogin.addEventListener('submit', prepareSendLogin);
+// }
 
-function prepareSendMail(e) {
-    e.preventDefault();
-    const data = {
-        name: formMail.name.value,
-        email: formMail.email.value,
-        text: formMail.text.value
-    };
-    const url = '/works';
+// function prepareSendMail(e) {
+//     e.preventDefault();
+//     const data = {
+//         name: formMail.name.value,
+//         email: formMail.email.value,
+//         text: formMail.text.value
+//     };
+//     const url = '/works';
 
-    prepareSend(url, formMail, data);
-}
+//     prepareSend(formMail, url, data);
+// }
 
-function prepareSendLogin(e) {
-    e.preventDefault();
-    const data = {
-        login: formLogin.login.value,
-        password: formLogin.password.value
-    };
-    console.log(data);
-    const url = '/index';
+// function prepareSendLogin(e) {
+//     e.preventDefault();
+//     const data = {
+//         login: formLogin.login.value,
+//         password: formLogin.password.value
+//     };
+//     console.log(data);
+//     const url = '/index';
 
-    prepareSend(url, formLogin, data, 'POST', data => {
-        if (data === 'Авторизация успешна!') {
-            location.href = '/admin';
-        }
-    });
-}
+//     prepareSend(url, formLogin, data, 'POST', data => {
+//         if (data === 'Авторизация успешна!') {
+//             location.href = '/admin';
+//         }
+//     });
+// }
